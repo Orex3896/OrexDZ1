e345b56505c40ad382103950154b276156c8e5f0
Vlad Oreshko <orex3896@gmail.com>
2022-12-01 20:25:08 +0300

    Create task #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int A = 5;
+int B = 6;
+int max = A;
+
+if (A>max) max = A;
+if (B>max) max = B;
+
+Console.WriteLine(max); 
